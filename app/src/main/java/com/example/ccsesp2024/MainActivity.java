dee391808a10bc84ba34442679be9a266554e08e
Tenealle <tenealleslo@gmail.com>
2024-03-08 10:23:26 -0600

    aaaaaa

--- app/src/main/java/com/example/ccsesp2024/MainActivity.java
+++ app/src/main/java/com/example/ccsesp2024/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.ccsesp2024;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), NameActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
