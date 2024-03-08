93c0ff2a5600f81339edf9468fcc88a9420142ca
Arian Baishya <arian.baishya2004@gmail.com>
2024-03-08 13:07:48 -0600

    Work day (3/8/2024)

--- app/src/main/java/com/example/ccsesp2024/MainActivity.java
+++ app/src/main/java/com/example/ccsesp2024/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.ccsesp2024;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +19,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button homeButton = findViewById(R.id.homeButton);
+        Button addButton = findViewById(R.id.addButton);
+        ImageView post = findViewById(R.id.post1);
+
+        // Registers a photo picker activity launcher in single-select mode.
+        ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+                registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                    // Callback is invoked after the user selects a media item or closes the
+                    // photo picker.
+                    if (uri != null) {
+                        Log.d("PhotoPicker", "Selected URI: " + uri);
+                        post.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                        post.setImageURI(uri);
+
+                    } else {
+                        Log.d("PhotoPicker", "No media selected");
+                    }
+                });
+        homeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent myIntent = new Intent(v.getContext(), NameActivity.class);
+                Intent myIntent = new Intent(v.getContext(), SettingsActivity.class);
                 startActivity(myIntent);
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Launch the photo picker and let the user choose only images.
+                pickMedia.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                        .build());
             }
         });
     }
