6a012cc77aeb2a2bf529e8a4c2ebe9a20ae6aed2
Arian Baishya <arian.baishya2004@gmail.com>
2024-03-08 13:53:40 -0600

    Update java version, add Retrofit REST API library, and reformat some files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,18 +23,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
     }
 }
 
 dependencies {
-
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.preference:preference:1.2.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity:1.8.2")
+    implementation("androidx.preference:preference:1.2.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
