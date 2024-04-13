518c888645def942d34b835e7fb6918f94196719
Arian Baishya <arian.baishya2004@gmail.com>
2024-04-12 21:07:54 -0500

    Clean up the app before open house - make like button work - implement share button - polished settings page a bit - add floating action button for adding posts - removed now-unused bottom bar - add top action bar

--- app/src/main/java/com/example/ccsesp2024/MainActivity.java
+++ app/src/main/java/com/example/ccsesp2024/MainActivity.java
@@ -5,8 +5,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -16,6 +17,8 @@ import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -23,25 +26,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button settingsButton = findViewById(R.id.settingsButton);
-        Button homeButton = findViewById(R.id.homeButton); // for funsies ig
-        Button addButton = findViewById(R.id.addButton);
+        FloatingActionButton addButton = findViewById(R.id.addPostButton);
 
         LinearLayout postsLayout = findViewById(R.id.postsLayout);
 
         // Registers a photo picker activity launcher in single-select mode.
         ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
-                registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), imageURI -> {
                     final Context CONTEXT = getApplicationContext();
                     final int ICON_SIZE = 125;
 
                     // Callback is invoked after the user selects a media item or closes the
                     // photo picker.
-                    if (uri != null) {
-                        Log.d("PhotoPicker", "Selected URI: " + uri);
+                    if (imageURI != null) {
+                        Log.d("PhotoPicker", "Selected URI: " + imageURI);
                         ImageView newPost = new ImageView(CONTEXT);
                         newPost.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                        newPost.setImageURI(uri);
+                        newPost.setImageURI(imageURI);
                         newPost.setAdjustViewBounds(true);
                         newPost.setMaxWidth(2000);
                         newPost.setMaxHeight(2000);
@@ -54,32 +55,40 @@ public class MainActivity extends AppCompatActivity {
                         ImageButton likeButton = new ImageButton(CONTEXT);
                         likeButton.setImageResource(R.drawable.heart);
                         likeButton.setColorFilter(Color.WHITE);
+                        likeButton.setBackgroundColor(Color.TRANSPARENT);
                         likeButton.setAdjustViewBounds(true);
                         likeButton.setMaxWidth(ICON_SIZE);
                         likeButton.setMaxHeight(ICON_SIZE);
-                        likeButton.setBackgroundColor(Color.TRANSPARENT);
                         likeButton.setClickable(true);
-                        likeButton.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                ImageButton thisButton = (ImageButton) view;
-                                Log.d("", String.format("button state: %s", thisButton.isActivated()));
+                        likeButton.setOnClickListener(view -> {
+                            ImageButton thisButton = (ImageButton) view;
+                            Log.d("", String.format("button state: %s", thisButton.isActivated()));
 
-                                if (thisButton.isActivated()) {
-                                    likeButton.setImageResource(R.drawable.heart_filled);
-                                    likeButton.setColorFilter(Color.RED);
-                                } else {
-                                    likeButton.setImageResource(R.drawable.heart);
-                                    likeButton.setColorFilter(Color.WHITE);
-                                }
+                            if (thisButton.isActivated()) {
+                                likeButton.setColorFilter(Color.WHITE);
+                                likeButton.setImageResource(R.drawable.heart);
+                                thisButton.setActivated(false);
+                            } else {
+                                likeButton.setImageResource(R.drawable.heart_filled);
+                                likeButton.setColorFilter(Color.RED);
+                                thisButton.setActivated(true);
                             }
                         });
 
                         ImageButton shareButton = new ImageButton(CONTEXT);
                         shareButton.setImageResource(android.R.drawable.ic_menu_share);
+                        shareButton.setColorFilter(Color.WHITE);
+                        shareButton.setBackgroundColor(Color.TRANSPARENT);
                         shareButton.setAdjustViewBounds(true);
                         shareButton.setMaxWidth(ICON_SIZE);
                         shareButton.setMaxHeight(ICON_SIZE);
+                        shareButton.setOnClickListener(v -> {
+                            Intent shareIntent = new Intent();
+                            shareIntent.setAction(Intent.ACTION_SEND);
+                            shareIntent.putExtra(Intent.EXTRA_STREAM, imageURI);
+                            shareIntent.setType("image/png");
+                            startActivity(Intent.createChooser(shareIntent, null));
+                        });
 
                         buttonLayout.addView(likeButton);
                         buttonLayout.addView(shareButton);
@@ -92,29 +101,28 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("PhotoPicker", "No media selected");
                     }
                 });
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(v.getContext(), SettingsActivity.class);
-                startActivity(myIntent);
-            }
-        });
 
-        addButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Launch the photo picker and let the user choose only images.
-                pickMedia.launch(new PickVisualMediaRequest.Builder()
-                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
-                        .build());
-            }
+        addButton.setOnClickListener(v -> {
+            // Launch the photo picker and let the user choose only images.
+            pickMedia.launch(new PickVisualMediaRequest.Builder()
+                    .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                    .build());
         });
+    }
 
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Launch the photo picker and let the user choose only images.
-                pickMedia.launch(new PickVisualMediaRequest.Builder()
-                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
-                        .build());
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menuSettings) {
+            startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
