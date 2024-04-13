bf0e7192ae6e209080a935e81e33fc5d58c9888d
Arian Baishya <arian.baishya2004@gmail.com>
2024-04-12 21:57:38 -0500

    remove unused dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity:1.8.2")
     implementation("androidx.preference:preference:1.2.1")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
